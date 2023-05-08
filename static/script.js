43bc56798d4718190d60dade8078dd79b68d7e12
maxime.guyon <maxime.guyon@photospace.fr>
2023-05-08 20:48:37 +0200

    - Cleanup to select mode

--- static/script.js
+++ static/script.js
@@ -3,12 +3,6 @@ $(document).ready(function() {
     var toastStatus = $("#toastStatus");
     var toastText = document.getElementById("toastText");
 
-    // Cache buster added because caching was a big problem on mobile
-    let cacheBuster = new Date().getTime();
-
-    let visualizationMode = 'chase';
-    let chosenColor = '';
-
     // Enable input spinner on all number type
     var props = {
         buttonsClass: "btn-primary"
@@ -40,40 +34,53 @@ $(document).ready(function() {
         copy: false,
     });
 
-    $('.selectMode').on('click', e => {
+    function updateMode(mode) {
+
+        console.log(mode);
+
+        // Reset all buttons
+        $("button[name=btnMode]").removeClass('btn-primary').addClass('btn-secondary');
+
+        // Set current button to active
+        $("button[name=btnMode][data-mode=" + mode + "]").removeClass('btn-secondary').addClass('btn-primary');
+
+        // Fetch music
+        if(mode == 'music') {
+            $.get('/list-sound', function(data) { 
+                // Clear the existing list
+                $('#soundList .list-group li').remove();
+
+                // Fill with retrived list
+                $.each(data.data, function(index, props) {
+                    let li = $("<li>")
+                        .attr('class', 'list-group-item')
+                        .data('props', JSON.stringify(props))
+                        .text(props.name)
+
+                    if(index === 0) {
+                        li.addClass('active')
+                    }
+
+                    $('#soundList .list-group').append(li)
+                });
+
+                // Click handler
+                $('.list-group li').click(function(e) {
+                    e.preventDefault()
+            
+                    $(this).parent().find('li').removeClass('active');
+                    $(this).addClass('active');
+                });
+            });
+        }
+    }
+
+    updateMode("text")
+
+    $("button[name=btnMode]").on('click', e => {
         let el = $(e.target);
         let mode = el.data('mode');
-        $('.selectMode').removeClass('btn-primary').addClass('btn-secondary');
-        el.removeClass('btn-secondary').addClass('btn-primary');
-        visualizationMode = mode;
-        console.log(visualizationMode);
-
-        $.get('/list-sound', function(data) { 
-            // Clear the existing list
-            $('#soundList .list-group li').remove();
-
-            // Fill with retrived list
-            $.each(data.data, function(index, props) {
-                let li = $("<li>")
-                    .attr('class', 'list-group-item')
-                    .data('props', JSON.stringify(props))
-                    .text(props.name)
-
-                if(index === 0) {
-                    li.addClass('active')
-                }
-
-                $('#soundList .list-group').append(li)
-            });
-
-            // Click handler
-            $('.list-group li').click(function(e) {
-                e.preventDefault()
-        
-                $(this).parent().find('li').removeClass('active');
-                $(this).addClass('active');
-            });
-        });
+        updateMode(mode)        
     });
 
     $('#text').keypress(function(event){
@@ -88,7 +95,7 @@ $(document).ready(function() {
         if(text) {
             let params = {
                 'text': text,
-                'duration': parseInt($('#duration').val())  || null,
+                'duration': parseInt($('#duration').val()) || null,
                 'color': alwanColorPicker.getColor().rgb(),
                 'color_mode': $("input[name=radioBtnColor]:checked").val(),
                 'color_intensity': $("input[name=radioBtnIntensity]:checked").val(),
